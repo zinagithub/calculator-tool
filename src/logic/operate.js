2c70069967753289c8a9e82bc2e97bc0a0509597
Talaa Zina <talazina@gmail.com>
2019-10-15 14:26:33 +0100

    add operate module

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,4 +1,19 @@
 import Big from 'big.js'
-export default function evaluate(nbr1, nbr2 , op){
 
+export default function evaluate(nbr1, nbr2 , op){
+   switch (op) {
+    case '+':
+      return Big (nbr1).plus(nbr2).toString()
+    case '-': 
+      return Big (nbr1).minus(nbr2).toString()
+    case '/':
+      if (nbr2 === '0') return 'Division By 0'
+      return Big (nbr1).div(nbr2).toString()
+    case 'x':
+      return Big (nbr1).times(nbr2).toString()
+    case '%':
+      return Big (nbr1).div(100).times(nbr2).toString()
+    default:
+      break
+  }
 }
